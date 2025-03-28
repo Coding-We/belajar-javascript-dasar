c379b8aee09b94bd45b773cf12cd9a35583626d1
Coding-We <santosso.sjim@gmail.com>
2025-03-28 14:37:51 +0700

    Pertemuan satu mejelaskan else if statement

---
+++ js/pertemuan2.js
@@ -0,0 +1,33 @@
+// else if statement
+
+// const iniHari = "senin";
+
+// if (iniHari === "senin") {
+//   // true
+//   console.log("Selamat Bekerja");
+// } else if (iniHari === "selasa") {
+//   // false
+//   console.log("Selasa");
+// } else if (iniHari === "rabu") {
+//   // false
+//   console.log("Rabu");
+// }
+
+const nilai = 60;
+
+if (nilai >= 80) {
+  // true
+  console.log("Nilai A");
+} else if (nilai >= 70) {
+  // false
+  console.log("Nilai B");
+} else if (nilai >= 60) {
+  // false
+  console.log("Nilai C");
+} else if (nilai >= 50) {
+  // false
+  console.log("Nilai D");
+} else {
+  // false
+  console.log("Nilai E");
+}
