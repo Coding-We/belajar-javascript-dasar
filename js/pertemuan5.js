8a14080fc9121957f6d37c62d5f690c3e1607f2b
Coding-We <santosso.sjim@gmail.com>
2025-03-28 15:13:58 +0700

    oprator logic and

---
+++ js/pertemuan5.js
@@ -0,0 +1,13 @@
+// oprator logic and
+// && adalah operator logic and
+
+const umur = prompt("Masukkan umur");
+const minUmur = 19;
+
+if (umur >= minUmur && umur <= 30) {
+  // true
+  console.log("Umur Benar memenuhi syarat");
+} else {
+  // false
+  console.log("Umur Salah tidak memenuhi syarat");
+}
