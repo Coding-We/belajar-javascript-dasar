55b29184c5ad7c13877b4398c6a8a24ea6432d63
Coding-We <santosso.sjim@gmail.com>
2025-03-28 14:36:26 +0700

    Pertemuan satu mejelaskan if statement

---
+++ js/pertemuan1.js
@@ -0,0 +1,25 @@
+// console.log("Hello World");
+// console.log("Belajar Javascript Dasar");
+// console.log("Belajar Javascript Dasar");
+// untuk menampilkan pesan di console
+
+// if statement
+// console.log("di luar if statement");
+
+// if (1 + 2 === 3) {
+//   // true
+//   console.log("di dalam if statement");
+// }
+
+// console.log("setelah if statement");
+
+let math = Math.random();
+console.log(math);
+
+if (math >= 0.5) {
+  console.log("lebih besar dari 0.5");
+}
+
+if (math <= 0.5) {
+  console.log("lebih kecil dari 0.5");
+}
