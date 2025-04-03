29ba22f702174fe9f2f6216565be4d959b902b7a
Coding-We <santosso.sjim@gmail.com>
2025-04-03 14:28:53 +0700

    Method Array Push dan Pop

---
+++ js/pertemuan10.js
@@ -0,0 +1,15 @@
+// Method Array
+// push adalah method untuk menambahkan nilai ke dalam array
+// pop adalah method untuk menghapus nilai dari array
+
+const barbel = [];
+
+barbel.pop("Kuning", "Putih", "Biru", "Hijau");
+barbel.push("Merah", "Hitam", "Biru");
+barbel.push("Hijau", "Biru");
+
+console.log(barbel);
+
+barbel.pop();
+
+console.log(barbel);
