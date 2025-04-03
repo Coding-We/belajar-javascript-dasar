cd6d183c3eaccfb7ca1ab3c2c489eaaabef72cc1
Coding-We <santosso.sjim@gmail.com>
2025-04-03 15:57:53 +0700

    Mengubah Nilai Const Dengan Array

---
+++ js/pertemuan14.js
@@ -0,0 +1,7 @@
+// Mengubah Nilai Const dengan Array
+const kumpulanWarna = ["Merah", "Kuning", "Hijau"];
+kumpulanWarna[0] = "Biru";
+kumpulanWarna[1] = "Putih";
+kumpulanWarna[2] = "Hitam";
+
+console.log(kumpulanWarna);
