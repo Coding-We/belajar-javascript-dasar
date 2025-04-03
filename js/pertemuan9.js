408ea71ce6313b38b3dde3c61f7516577149205b
Coding-We <santosso.sjim@gmail.com>
2025-04-03 14:17:59 +0700

    Mendapatkan nilai dengan index dan mengubah isinya

---
+++ js/pertemuan9.js
@@ -0,0 +1,14 @@
+// Mengenal struktur data array
+
+// Deklarasi array
+// Mendapatkan Nilai dari array
+
+let warna = ["Merah", "Kuning", "Hijau"];
+
+warna[0] = "Biru";
+warna[1] = "Putih";
+warna[2] = "Hitam";
+
+console.log(warna);
+
+console.log(warna[2]);
