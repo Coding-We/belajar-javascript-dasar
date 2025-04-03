d216deaa25126348dc504fe967d379df8f4b0faa
Coding-We <santosso.sjim@gmail.com>
2025-04-03 15:43:01 +0700

    Beberapa Method Array Yang Sering Di Guanakan

---
+++ js/pertemuan12.js
@@ -0,0 +1,15 @@
+// Method array yang sering dipakai
+// join adalah method untuk menggabungkan array menjadi string
+// slice adalah method untuk memotong array
+// reverse adalah method untuk membalik array
+
+const students = ["Eko", "Kurniawan", "Khannedy"];
+
+const join = students.join(",");
+console.log(join);
+
+const slice = students.slice(1, 3);
+console.log(slice);
+
+const reverse = students.reverse();
+console.log(reverse);
