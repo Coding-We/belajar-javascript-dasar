0f01be7b5aea5f967a1366d12d578fb235542787
Coding-We <santosso.sjim@gmail.com>
2025-04-03 22:01:31 +0700

    Cara Membuat Struktur Data Object

---
+++ js/pertemuan16.js
@@ -0,0 +1,23 @@
+// Apa itu object
+// Object adalah kumpulan data yang disimpan dalam satu variabel
+// Cara membuat struktur data object
+// Object banyak menampung data berbentuk key value
+
+const orang = {
+  nama: "Santoso",
+  pekerjaan: "Programmer",
+  umur: 20,
+};
+
+console.log(orang);
+
+const barang = {
+  nama: "Buku",
+  harga: 10000,
+  penerbit: "Gramedia",
+  stok: 10,
+  halaman: 200,
+  status: "Tersedia",
+};
+
+console.log(barang);
