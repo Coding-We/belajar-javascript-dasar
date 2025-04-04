b53549ae8010df23ec40426a58ac3a06903a5d1b
Coding-We <santosso.sjim@gmail.com>
2025-04-04 12:23:04 +0700

    Pengenalan perulangan

---
+++ js/pertemuan20.js
@@ -0,0 +1,16 @@
+// JavaScript Perulangan menggunakan for
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+// Menggunakan perulangan for
+for (let i = 1; i <= 10; i++) {
+  console.log("Perulangan ke-" + i);
+}
