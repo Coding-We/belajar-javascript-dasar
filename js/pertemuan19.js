5379b9a27b7337abb166a9b83a82bc09922f2d31
Coding-We <santosso.sjim@gmail.com>
2025-04-04 12:11:24 +0700

    Cara memanggil object di dalam array

---
+++ js/pertemuan19.js
@@ -0,0 +1,17 @@
+// Cara memanggil object di dalam array
+const mahasiswa = [
+  {
+    nama: "John Doe",
+    nim: "12345678",
+    jurusan: "Teknik Informatika",
+  },
+  {
+    nama: "Jane Smith",
+    nim: "87654321",
+    jurusan: "Sistem Informasi",
+  },
+];
+
+console.log(mahasiswa[0].nama); // Output: John Doe
+console.log(mahasiswa[1].nim); // Output: 87654321
+// Cara memanggil array di dalam object
