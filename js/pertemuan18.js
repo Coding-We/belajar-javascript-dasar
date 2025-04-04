42912a7a14d5a2a5939233d5525e520b3c9d8bb6
Coding-We <santosso.sjim@gmail.com>
2025-04-04 12:05:12 +0700

    Cara membuat object berjalan

---
+++ js/pertemuan18.js
@@ -0,0 +1,16 @@
+// Cara merubah data dari object
+// Cara membuat object berjalan
+
+const data = {
+  name: "John",
+  age: 30,
+  city: "New York",
+};
+
+// Mengubah data dari object
+data.name = "Jane";
+data.age = 25;
+data.city = "Los Angeles";
+data.gender = "Pria";
+
+console.log(data);
