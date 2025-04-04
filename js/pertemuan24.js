a6509a1a9d707b0f5437c46226898ffaf2e54e01
Coding-We <santosso.sjim@gmail.com>
2025-04-04 17:47:30 +0700

    Pengenalan Nested Loop

---
+++ js/pertemuan24.js
@@ -0,0 +1,10 @@
+// Pengenalan Nested Loop
+// Nested Loop adalah loop di dalam loop
+// Contoh Nested Loop
+
+for (let i = 1; i < 5; i++) {
+  console.log("Soal Nomor" + i);
+  for (let j = 0; j < 3; j++) {
+    console.log("Pilihan Jawaban" + j);
+  }
+}
