eac97750af5e29e61b1cc63a25af54b8301ca3a3
Coding-We <santosso.sjim@gmail.com>
2025-04-04 20:41:30 +0700

    Pengenalan Function Di JavaScript

---
+++ js/pertemuan31.js
@@ -0,0 +1,11 @@
+// Pengenalan Function di Javascript
+// Function adalah sebuah blok kode yang dapat dijalankan secara independen
+
+let randomNumber = Math.floor(Math.random() * 100) + 1; // Menghasilkan angka acak antara 1 dan 100
+
+console.log(randomNumber); // Menampilkan angka acak di konsol
+
+function lemparDadu() {
+  let dadu = Math.floor(Math.random() * 6) + 1; // Menghasilkan angka acak antara 1 dan 6
+  console.log(dadu); // Menampilkan angka dadu di konsol
+}
