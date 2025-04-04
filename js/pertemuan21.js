39257b27e9a105e0f92bee21a6f78f71a90b2ac3
Coding-We <santosso.sjim@gmail.com>
2025-04-04 17:18:49 +0700

    Contoh lain perulangan for

---
+++ js/pertemuan21.js
@@ -0,0 +1,13 @@
+// Contoh perulangan lain perulangan for
+
+// for (let i = 0; i < 10; i++) {
+//   console.log("Hello World " + i);
+// }
+// Contoh perulangan lain perulangan for
+for (let i = 20; i >= 0; i -= 2) {
+  console.log(i);
+}
+
+for (let i = 10; i <= 100000; i *= 10) {
+  console.log(i);
+}
