8124ef3faa2b01ac7c6c35729ad6014723116966
Coding-We <santosso.sjim@gmail.com>
2025-04-08 15:07:02 +0700

    Argument dan parameter dalam function

---
+++ js/pertemuan33.js
@@ -0,0 +1,8 @@
+// Argument dan parameter dalam function
+// Argument adalah nilai yang dikirimkan ke dalam function
+// Parameter adalah variabel yang digunakan untuk menampung nilai yang dikirimkan ke dalam function
+
+function greet(name) {
+  console.log(`Hi ${name} Selamat datang di JS`);
+}
+greet("John"); // Argument: "John", Parameter: name
